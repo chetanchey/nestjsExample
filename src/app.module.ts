fd71840548e34c148e2ce2593f4c4e9ba4fc62ea
chetanchey <atthulurichetan@gmail.com>
2024-10-08 01:47:20 -0500

    updated controllers & added books resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, BooksModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,38 @@
-import { Controller, Get, Param } from '@nestjs/common';
-import { get } from 'http';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
+  @Get() // GET /users or /users?role=value
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return [];
+  }
 
-    @Get()
-    findAll(){
-        return []
-    }
+  @Get(':id') // GET /users/:id
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: string){
-        return { id }
-    }
+  @Post() // POST /users
+  create(@Body() user: {}) {
+    return user;
+  }
+
+  @Patch(':id') // PATCH /users/:id
+  update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id') // DELETE /users/:id
+  delete(@Param('id') id: string) {
+    return { id };
+  }
 }
