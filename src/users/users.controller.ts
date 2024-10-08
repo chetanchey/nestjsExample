b338a589bd87bb39334a0d38d83279928f79e7f4
chetanchey <atthulurichetan@gmail.com>
2024-10-08 21:42:16 +0530

    updated services

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,38 +1,34 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-  Query,
-} from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-  @Get() // GET /users or /users?role=value
-  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
-    return [];
-  }
 
-  @Get(':id') // GET /users/:id
-  findOne(@Param('id') id: string) {
-    return { id };
-  }
+    constructor(private readonly usersService: UsersService) { }
 
-  @Post() // POST /users
-  create(@Body() user: {}) {
-    return user;
-  }
+    @Get() // GET /users or /users?role=value
+    findAll(@Query('role') role?: 'INTERN' | 'ENGINEER') {
+        return this.usersService.findAll(role)
+    }
 
-  @Patch(':id') // PATCH /users/:id
-  update(@Param('id') id: string, @Body() userUpdate: {}) {
-    return { id, ...userUpdate };
-  }
+    @Get(':id') // GET /users/:id
+    findOne(@Param('id') id: string) {
+        return this.usersService.findOne(+id)
+    }
 
-  @Delete(':id') // DELETE /users/:id
-  delete(@Param('id') id: string) {
-    return { id };
-  }
+    @Post() // POST /users 
+    create(@Body() user: { name: string, email: string, role: 'INTERN' | 'ENGINEER' }) {
+        return this.usersService.create(user)
+    }
+
+    @Patch(':id') // PATCH /users/:id
+    update(@Param('id') id: string, @Body() userUpdate: { name?: string, email?: string, role?: 'INTERN' | 'ENGINEER' }) {
+        return this.usersService.update(+id, userUpdate)
+    }
+
+    @Delete(':id') // DELETE /users/:id
+    delete(@Param('id') id: string) {
+        return this.usersService.delete(+id)
+    }
+
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,79 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users = [
+        {
+            "id": 1,
+            "name": "chetan",
+            "email": "chetan@gmail.com",
+            "role": "INTERN",
+        },
+        {
+            "id": 2,
+            "name": "lalit",
+            "email": "lalit@gmail.com",
+            "role": "INTERN",
+        },
+        {
+            "id": 3,
+            "name": "dharmi",
+            "email": "dharmi@gmail.com",
+            "role": "ENGINEER",
+        },
+        {
+            "id": 4,
+            "name": "syed",
+            "email": "syed@gmail.com",
+            "role": "ENGINEER",
+        },
+        {
+            "id": 5,
+            "name": "raju",
+            "email": "raju@gmail.com",
+            "role": "INTERN",
+        }
+    ]
+
+    findAll(role?: 'INTERN' | 'ENGINEER'){
+        if(role){
+        return this.users.filter(user => user.role === role)
+        }
+        return this.users
+    }
+
+    findOne(id : number){
+        const user = this.users.find(user => user.id === id)
+
+        return user
+    }
+
+    create(user: { name: string, email: string, role: 'INTERN' | 'ENGINEER' }) {
+        const usersByHighestId = [...this.users].sort((a, b) => b.id - a.id)
+        const newUser = {
+            id: usersByHighestId[0].id + 1,
+            ...user
+        }
+        this.users.push(newUser)
+        return newUser
+    }
+
+    update(id: number, updatedUser: { name?: string, email?: string, role?: 'INTERN' | 'ENGINEER' }) {
+        this.users = this.users.map(user => {
+            if (user.id === id) {
+                return { ...user, ...updatedUser }
+            }
+            return user
+        })
+
+        return this.findOne(id)
+    }
+
+    delete(id: number) {
+        const removedUser = this.findOne(id)
+
+        this.users = this.users.filter(user => user.id !== id)
+
+        return removedUser
+    }
+}
