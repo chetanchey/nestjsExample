5bfaa5fdfc2bc511f28deb102ac2beb14cd1aa64
chetanchey <atthulurichetan@gmail.com>
2024-10-09 21:33:35 +0530

    added env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { BooksModule } from './books/books.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule, BooksModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath:'.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DB_URI),
+    UsersModule,
+    BooksModule
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   controllers: [UsersController],
